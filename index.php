7d2c2798b091cf2756fbf2abfbd133f7efbd8c2e
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2026-02-06 11:27:27 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+</head>
+<body>
+
+<!-- ARRAY DI PARTENZA -->
+<?php 
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Splendid',
+        'description' => 'Hotel Splendid Descrizione',
+        'parking' => true,
+        'vote' => 5,
+        'distance_to_center' => 0.5
+    ],
+    [
+        'name' => 'Hotel Economico',
+        'description' => 'Hotel Economico Descrizione',
+        'parking' => false,
+        'vote' => 2,
+        'distance_to_center' => 15
+    ],
+    [
+        'name' => 'Hotel Centrale',
+        'description' => 'Hotel Centrale Descrizione',
+        'parking' => true,
+        'vote' => 3,
+        'distance_to_center' => 0.1
+    ],
+]
+?>
+
+    
+</body>
+</html>
