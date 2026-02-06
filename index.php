31f5fe3e2d7115a10d74928d4de19e4244edc701
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2026-02-06 12:39:04 +0100

    cicle with foreach method into the array and show into the page ul and li with the properties for each hotel

--- index.php
+++ index.php
@@ -1,60 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
 </head>
+
 <body>
 
-<!-- ARRAY DI PARTENZA -->
-<?php 
-$hotels = [
-    [
-        'name' => 'Hotel Belvedere',
-        'description' => 'Hotel Belvedere Descrizione',
-        'parking' => true,
-        'vote' => 4,
-        'distance_to_center' => 10.4
-    ],
-    [
-        'name' => 'Hotel Futuro',
-        'description' => 'Hotel Futuro Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 2
-    ],
-    [
-        'name' => 'Hotel Rivamare',
-        'description' => 'Hotel Rivamare Descrizione',
-        'parking' => false,
-        'vote' => 1,
-        'distance_to_center' => 1
-    ],
-    [
-        'name' => 'Hotel Splendid',
-        'description' => 'Hotel Splendid Descrizione',
-        'parking' => true,
-        'vote' => 5,
-        'distance_to_center' => 0.5
-    ],
-    [
-        'name' => 'Hotel Economico',
-        'description' => 'Hotel Economico Descrizione',
-        'parking' => false,
-        'vote' => 2,
-        'distance_to_center' => 15
-    ],
-    [
-        'name' => 'Hotel Centrale',
-        'description' => 'Hotel Centrale Descrizione',
-        'parking' => true,
-        'vote' => 3,
-        'distance_to_center' => 0.1
-    ],
-]
-?>
+    <!-- ARRAY DI PARTENZA -->
+    <?php
 
-    
-</body>
-</html>
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    ?>
+    <?php
+
+    // CICLO PER PRENDERE OGNI HOTEL E STAMPARE LE SUE PROPRIETà
+    foreach ($hotels as $hotel)
+        echo
+        "<ul>
+        <li><h1>$hotel[name]</h1></li>
+        <li><p>Description: $hotel[description]</p></li>
+        <li><p>Parking: $hotel[parking]</p></li>
+        <li><p>Vote: $hotel[vote]</p></li>
+        <li><p>Distance to center: $hotel[distance_to_center]</p></li>
+        </ul>"
+        ?>
+
+    </body>
+
+    </html>
