ede4843df6a4052acc6d70c1a3b32cfe128cb343
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2026-02-06 14:50:07 +0100

    import bootstrap and completed bootstrap table implementation and set the condition to the text of the parking field

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
 
-    <!-- ARRAY DI PARTENZA -->
     <?php
-
+    //  ARRAY DI PARTENZA
     $hotels = [
 
         [
@@ -53,20 +54,39 @@
     ];
 
     ?>
-    <?php
+    <div class="container">
+        <h1 class="text-center p-4">Hotels in the city</h1>
 
-    // CICLO PER PRENDERE OGNI HOTEL E STAMPARE LE SUE PROPRIETà
-    foreach ($hotels as $hotel)
-        echo
-        "<ul>
-        <li><h1>$hotel[name]</h1></li>
-        <li><p>Description: $hotel[description]</p></li>
-        <li><p>Parking: $hotel[parking]</p></li>
-        <li><p>Vote: $hotel[vote]</p></li>
-        <li><p>Distance to center: $hotel[distance_to_center]</p></li>
-        </ul>"
+        <ul class="list-group list-group-horizontal fw-bold rounded-0">
+            <li class="list-group-item rounded-0 col-2">Name</li>
+            <li class="list-group-item col-4">Description</li>
+            <li class="list-group-item col-2">Parking</li>
+            <li class="list-group-item col-2">Vote</li>
+            <li class="list-group-item rounded-0 col-2">Distance</li>
+        </ul>
+
+        <?php
+        // Avvio un ciclo sull'array
+        foreach ($hotels as $hotel) {
+            //Condizione per il parcheggio
+            if ($hotel["parking"]) {
+                $parking = "Yes";
+            } else {
+                $parking = "No";
+            }
+            // Stampo in pagina la card
+            echo "
+            <ul class='list-group list-group-horizontal rounded-0'>
+                <li class='list-group-item rounded-0 col-2'>$hotel[name]</li>
+                <li class='list-group-item col-4'>$hotel[description]</li>
+                <li class='list-group-item col-2'>$parking</li>
+                <li class='list-group-item col-2'>$hotel[vote]/5</li>
+                <li class='list-group-item rounded-0 col-2'>$hotel[distance_to_center] km</li>
+            </ul>
+        ";
+        }
         ?>
+    </div>
+</body>
 
-    </body>
-
-    </html>
+</html>
